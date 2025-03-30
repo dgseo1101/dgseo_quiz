5dc9766d06268e19888b8479ae758941f23f833e
dgseo1101 <nohes8509@gmail.com>
2025-03-30 19:14:29 +0900

    fix : assignment 권한검증 추가

--- server/application/controllers/assignment_controller.py
+++ server/application/controllers/assignment_controller.py
@@ -11,6 +11,9 @@ from core.application.dtos.assignment_dto import (
 from server.application.services.assignment_service import AssignmentService
 from server.infrastructure.di.container import ServerContainer
 
+from server.shard_kernel.auth_helper import validate_admin_and_get_user_id
+
+
 router = APIRouter(prefix="/assignment", tags=["응시자 관리"])
 
 @router.get("")
@@ -18,22 +21,16 @@ router = APIRouter(prefix="/assignment", tags=["응시자 관리"])
 async def get_assignments(
     page: int = 1,
     page_size: int = 10,
+    user_id: int = Depends(validate_admin_and_get_user_id),
     assignment_service: AssignmentService = Depends(Provide[ServerContainer.assignment_service])
 )->List[AssignmentResponseDto]:
     return await assignment_service.get_datas(page=page, page_size=page_size)
-
-@router.get("/{assignment_id}")
-@inject
-async def get_assignment(
-    assignment_id: int,
-    assignment_service: AssignmentService = Depends(Provide[ServerContainer.assignment_service])
-)->AssignmentResponseDto:
-    return await assignment_service.get_data_by_data_id(data_id=assignment_id)
 
 @router.post("")
 @inject
 async def create_assignment(
     create_data: CreateAssignmentRequestDto,
+    user_id: int = Depends(validate_admin_and_get_user_id),
     assignment_service: AssignmentService = Depends(Provide[ServerContainer.assignment_service])
 )->AssignmentResponseDto:
     return await assignment_service.create_data(create_data=create_data)
@@ -43,6 +40,7 @@ async def create_assignment(
 async def update_assignment(
     assignment_id: int,
     update_data: UpdateAssignmenRequestDto,
+    user_id: int = Depends(validate_admin_and_get_user_id),
     assignment_service: AssignmentService = Depends(Provide[ServerContainer.assignment_service])
 )->AssignmentResponseDto:
     return await assignment_service.update_data_by_data_id(data_id=assignment_id, update_data=update_data)
@@ -51,6 +49,7 @@ async def update_assignment(
 @inject
 async def delete_assignment(
     assignment_id: int,
+    user_id: int = Depends(validate_admin_and_get_user_id),
     assignment_service: AssignmentService = Depends(Provide[ServerContainer.assignment_service])
 )->int:
     await assignment_service.delete_data_by_data_id(data_id=assignment_id)
